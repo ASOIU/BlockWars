3081a402019d95b739c05a1ed4a3fe25efdee5d7
DennisBorisov <dennisborisov94@gmail.com>
2013-08-17 02:31:14 +0700

    Added method which building additional blocks for base

--- BlockWars/BlockWars/Gameplay/PlayerBaseFactory.cs
+++ BlockWars/BlockWars/Gameplay/PlayerBaseFactory.cs
@@ -14,12 +14,15 @@ namespace BlockWars.Gameplay
         public static List<Box> CreateBuilding(World world, Gameplay gameplay, EntityCategory playerType)
         {
             List<Box> boxes = new List<Box>();
+            Terrain terrain = new Terrain(world);
+            bool flag = true;
+
             switch (playerType)
             {
                 case (EntityCategory.Player1):
                     {
                         float x, y;
-                        y = -18f;
+                        y = terrain.GetHeight(-300);
                         float bw, bh;
                         bw = 6;
                         bh = 3;
@@ -31,6 +34,11 @@ namespace BlockWars.Gameplay
                             x = -300;
                             for (int j = 0; j < 3; j++)
                             {
+                                if (j >= 1 && i>=1)
+                                {
+                                    flag = false;
+                                }
+                                CreateNew(flag, terrain, x, y, bw, bh, boxes, world, gameplay);
                                 position = new Vector2(x, y);
                                 size = new Vector2(bw, bh);
                                 box = new Box(world, position, size, "base-block", true, gameplay.Player1, BASE_STRENGTH);
@@ -39,8 +47,9 @@ namespace BlockWars.Gameplay
                             }
                             y += bh;
                         }
+                        flag = true;
                         x = -303;
-                        y = 5.8f;
+                        y = terrain.GetHeight(x) + 24f;
                         for (int i = 0; i < 4; i++)
                         {
                             position = new Vector2(x, y);
@@ -50,7 +59,7 @@ namespace BlockWars.Gameplay
                             x += bw;
                         }
                         x = -303;
-                        y = 8.8f;
+                        y = terrain.GetHeight(x) + 27f;
                         for (int i = 0; i < 3; i++)
                         {
                             position = new Vector2(x, y);
@@ -59,12 +68,17 @@ namespace BlockWars.Gameplay
                             boxes.Add(box);
                             x += bw + bw / 2f;
                         }
-                        y = -18f;
+                        y = terrain.GetHeight(-230);
                         for (int i = 0; i < 8; i++)
                         {
                             x = -230;
                             for (int j = 0; j < 3; j++)
                             {
+                                if (j >= 1 && i >= 1)
+                                {
+                                    flag = false;
+                                }
+                                CreateNew(flag, terrain, x, y, bw, bh, boxes, world, gameplay);
                                 position = new Vector2(x, y);
                                 size = new Vector2(bw, bh);
                                 box = new Box(world, position, size, "block2", true, gameplay.Player1,200);
@@ -74,7 +88,7 @@ namespace BlockWars.Gameplay
                             y += bh;
                         }
                         x = -233;
-                        y = 5.8f;
+                        y = terrain.GetHeight(-233)+24f;
                         for (int i = 0; i < 4; i++)
                         {
                             position = new Vector2(x, y);
@@ -84,7 +98,7 @@ namespace BlockWars.Gameplay
                             x += bw;
                         }
                         x = -233;
-                        y = 8.8f;
+                        y = terrain.GetHeight(-233)+27f;
                         for (int i = 0; i < 3; i++)
                         {
                             position = new Vector2(x, y);
@@ -93,12 +107,18 @@ namespace BlockWars.Gameplay
                             boxes.Add(box);
                             x += bw + bw / 2f;
                         }
-                        y = -18f;
+                        flag = true;
+                        y = terrain.GetHeight(-236);
                         for (int i = 0; i < 5; i++)
                         {
                             x = -236;
                             for (int j = 0; j < 8; j++)
                             {
+                                if (j >= 1 && i >= 1)
+                                {
+                                    flag = false;
+                                }
+                                CreateNew(flag, terrain, x, y, bw, bh, boxes, world, gameplay);
                                 position = new Vector2(x, y);
                                 size = new Vector2(bw, bh);
                                 box = new Box(world, position, size, "block3", true, gameplay.Player1);
@@ -106,13 +126,14 @@ namespace BlockWars.Gameplay
                                 x -= bw;
                             }
                             y += bh;
-                        } 
+                        }
+                        flag = true;
                         break;
                     }
                 case (EntityCategory.Player2):
                     {
                         float x, y;
-                        y = -18f;
+                        y = terrain.GetHeight(300);
                         float bw, bh;
                         bw = 6;
                         bh = 3;
@@ -133,7 +154,7 @@ namespace BlockWars.Gameplay
                             y += bh;
                         }
                         x = 303;
-                        y = 5.8f;
+                        y = terrain.GetHeight(300)+24f;
                         for (int i = 0; i < 4; i++)
                         {
                             position = new Vector2(x, y);
@@ -143,7 +164,7 @@ namespace BlockWars.Gameplay
                             x -= bw;
                         }
                         x = 303;
-                        y = 8.8f;
+                        y = terrain.GetHeight(300)+27f;
                         for (int i = 0; i < 3; i++)
                         {
                             position = new Vector2(x, y);
@@ -152,7 +173,7 @@ namespace BlockWars.Gameplay
                             boxes.Add(box);
                             x -= bw + bw / 2f;
                         }
-                        y = -18f;
+                        y = terrain.GetHeight(230);
                         for (int i = 0; i < 8; i++)
                         {
                             x = 230;
@@ -167,7 +188,7 @@ namespace BlockWars.Gameplay
                             y += bh;
                         }
                         x = 233;
-                        y = 5.8f;
+                        y = terrain.GetHeight(233)+24f;
                         for (int i = 0; i < 4; i++)
                         {
                             position = new Vector2(x, y);
@@ -177,7 +198,7 @@ namespace BlockWars.Gameplay
                             x -= bw;
                         }
                         x = 233;
-                        y = 8.8f;
+                        y = terrain.GetHeight(233)+27f;
                         for (int i = 0; i < 3; i++)
                         {
                             position = new Vector2(x, y);
@@ -186,7 +207,7 @@ namespace BlockWars.Gameplay
                             boxes.Add(box);
                             x -= bw + bw / 2f;
                         }
-                        y = -18f;
+                        y = terrain.GetHeight(236);
                         for (int i = 0; i < 5; i++)
                         {
                             x = 236;
@@ -204,6 +225,37 @@ namespace BlockWars.Gameplay
                     }
             }
             return boxes;
+        }
+
+        private static void CreateNew(bool flag, Terrain terrain, float x, float y, float bw, float bh, List<Box> boxes, World world, Gameplay gameplay)
+        {
+            Vector2 position;
+            Vector2 size;
+            Box box;
+            if (flag)
+            {
+                float checkY;
+                int number;
+                checkY = terrain.GetHeight(x);
+                if (checkY != y)
+                {
+                    checkY = y - checkY;
+                    number = (int)(checkY / 3);
+                    Console.WriteLine(number);
+                    float newY;
+                    newY = checkY;
+                    for (int k = 1; k <= number; k++)
+                    {
+                        newY += bh;
+                        Console.WriteLine(newY);
+                        position = new Vector2(x, newY);
+                        size = new Vector2(bw, bh);
+                        box = new Box(world, position, size, "base-block", true, gameplay.Player1, BASE_STRENGTH);
+                        boxes.Add(box);
+                        Console.WriteLine("Yes!");
+                    }
+                }
+            }
         }
     }
 }
