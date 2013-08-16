b6a2aac4d3fd7e6a3d3c367bfeb972cef126dadd
Victor <victor99992@yandex.ru>
2013-08-16 12:14:42 +0600

    Added formulas to Terrain.cs

--- BlockWars/BlockWars/Terrain.cs
+++ BlockWars/BlockWars/Terrain.cs
@@ -7,87 +7,98 @@ using Box2D.XNA;
 
 namespace BlockWars
 {
-	class Terrain:AGameObject
-	{
-		float[] mTerrainBaseVertexs;
-		Vector2[] mTerrainDrawVertexs;
-		int mScale = 25;
+    class Terrain : AGameObject
+    {
+        float[] mTerrainBaseVertexs;
+        Vector2[] mTerrainDrawVertexs;
+        int mScale = 25;
+        int mCountBaseVertexs = 100; //-3
+        int mAmplitude = 800;
         BodyDef mBodyDef;
         Body mBody;
-		public Terrain(World world)
-		{
+        public Terrain(World world)
+        {
             mBodyDef = new BodyDef();
             mBody = world.CreateBody(mBodyDef);
-			Generate();
-			GenerateDrawVertexs();
-		}
+            Generate();
+            GenerateDrawVertexs();
+        }
 
-		private void GenerateDrawVertexs()
-		{
-			mTerrainDrawVertexs = new Vector2[70];
-			for (int i = 1; i < 5; i++)
-			{
-				for (int j = 0; j < 10; j++)
-				{
-					mTerrainDrawVertexs[i * 10 + j] = new Vector2((i-1) * 10*mScale + j*mScale, CubicInterpolate(mTerrainBaseVertexs[i-1], mTerrainBaseVertexs[i], mTerrainBaseVertexs[i + 1], mTerrainBaseVertexs[i + 2], j / 10f));
-				}
-			}
-            for (int i = 0; i < 70; i++)
+        private void GenerateDrawVertexs()
+        {
+            Vector2[] vertexs = new Vector2[mCountBaseVertexs * 10];
+            for (int i = 1; i < mCountBaseVertexs - 2; i++)
             {
-                mTerrainDrawVertexs[i].X -= 487.5f;
+                for (int j = 0; j < 10; j++)
+                {
+                    float x = (i - 1) * 10 * mScale + j * mScale;
+                    float y = CubicInterpolate(
+                              mTerrainBaseVertexs[i - 1],
+                              mTerrainBaseVertexs[i],
+                              mTerrainBaseVertexs[i + 1],
+                              mTerrainBaseVertexs[i + 2], j / 10f);
+                    vertexs[i * 10 + j] = new Vector2(x, y);
+                }
+            }
+            int count = (mCountBaseVertexs - 3) * 10;
+            mTerrainDrawVertexs = new Vector2[count + 3];
+            Array.Copy(vertexs, 10, mTerrainDrawVertexs, 0, count);
+            for (int i = 0; i < count; i++)
+            {
+                mTerrainDrawVertexs[i].X -= ((mCountBaseVertexs - 4) * 10 * mScale + 9 * mScale) / 2f;
                 mTerrainDrawVertexs[i].Y -= 50;
             }
-            for (int i = 10; i < 49; i++)
+            float y1 = -200;
+            mTerrainDrawVertexs[count] = new Vector2(mTerrainDrawVertexs[count - 1].X, y1);
+            mTerrainDrawVertexs[count + 1] = new Vector2(mTerrainDrawVertexs[0].X, y1);
+            mTerrainDrawVertexs[count + 2] = mTerrainDrawVertexs[0];
+
+            for (int i = 0; i < count - 1; i++)
             {
                 PolygonShape shape = new PolygonShape();
-				shape.SetAsEdge(mTerrainDrawVertexs[i], mTerrainDrawVertexs[i+1]);
-				mBody.CreateFixture(shape, 0.0f);
+                shape.SetAsEdge(mTerrainDrawVertexs[i], mTerrainDrawVertexs[i + 1]);
+                mBody.CreateFixture(shape, 0.0f);
             }
-		}
+        }
 
-		private void Generate()
-		{
-			Random r = new Random();
-			mTerrainBaseVertexs = new float[7];
-			for (int i = 0; i < 7; i++)
-			{
-				mTerrainBaseVertexs[i] = r.Next(0, 800) / 10f;
-			}
-		}
+        private void Generate()
+        {
+            Random r = new Random();
+            mTerrainBaseVertexs = new float[mCountBaseVertexs];
+            for (int i = 0; i < mCountBaseVertexs; i++)
+            {
+                mTerrainBaseVertexs[i] = r.Next(0, mAmplitude) / 10f;
+            }
+        }
 
-		float CubicInterpolate(double y0, double y1,double y2, double y3,double mu)
-		{
-			double a0, a1, a2, a3, mu2;
+        float CubicInterpolate(double y0, double y1, double y2, double y3, double mu)
+        {
+            double a0, a1, a2, a3, mu2;
 
-			mu2 = mu * mu;
-			a0 = y3 - y2 - y0 + y1;
-			a1 = y0 - y1 - a0;
-			a2 = y2 - y0;
-			a3 = y1;
+            mu2 = mu * mu;
+            a0 = y3 - y2 - y0 + y1;
+            a1 = y0 - y1 - a0;
+            a2 = y2 - y0;
+            a3 = y1;
 
-			return (float)(a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3);
-		}
-		public override void Draw(PrimitiveRender primitiveRender)
-		{
-			Vector2[] Vertexs = new Vector2[43];
-			Array.Copy(mTerrainDrawVertexs, 10, Vertexs, 0, 40);
-            float y = -200;
-            Vertexs[40] = new Vector2(Vertexs[39].X, y);
-            Vertexs[41] = new Vector2(Vertexs[0].X, y);
-            Vertexs[42] = Vertexs[0];
-			primitiveRender.DrawLineList(Vertexs, 43, Color.White);
-		}
-		public override void Destroy()
-		{
-			throw new NotImplementedException();
-		}
-		public override Vector2 GetPosition()
-		{
-			throw new NotImplementedException();
-		}
-		public override void SetPosition(Vector2 position)
-		{
-			throw new NotImplementedException();
-		}
-	}
+            return (float)(a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3);
+        }
+        public override void Draw(PrimitiveRender primitiveRender)
+        {
+            int count = (mCountBaseVertexs - 3) * 10 + 3;
+            primitiveRender.DrawLineList(mTerrainDrawVertexs, count, Color.White);
+        }
+        public override void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+        public override Vector2 GetPosition()
+        {
+            throw new NotImplementedException();
+        }
+        public override void SetPosition(Vector2 position)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
